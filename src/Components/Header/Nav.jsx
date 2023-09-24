f2134ae0f3c125117b3804af4e7c3b57bb4e09fb
redwan islam <redwan390309@gmail.com>
2023-09-24 21:23:38 +0600

    complete the main routing and add the nav section and banner

--- src/Components/Header/Nav.jsx
+++ src/Components/Header/Nav.jsx
@@ -3,14 +3,29 @@ import { NavLink } from "react-router-dom";
 
 const Nav = () => {
     return (
-        <div className="flex justify-between">
+        <div className="flex justify-between items-center py-3 max-w-6xl mx-auto">
          <div>
             <img src="https://i.ibb.co/XSv3CWQ/Logo.png" alt="" />
          </div>
-         <div >
-            <NavLink to="/"> Home</NavLink>
-            <NavLink to ="/donation"> Donation</NavLink>
-            <NavLink to="/statistics"> Statistics</NavLink>
+         <div  className="flex gap-3 font-bold text-xl">
+            <NavLink to="/" 
+              className={ ({ isActive, isPending ,} ) =>
+              isPending ? "pending" : isActive ? "text-red-600 underline " : ""
+              
+            }
+            
+            > Home</NavLink>
+            <NavLink to ="/donation"
+               className={({ isActive, isPending }) =>
+               isPending ? "pending" : isActive ? "text-red-700 underline " : ""
+             }
+            > Donation</NavLink>
+
+            <NavLink to="/statistics"
+               className={({ isActive, isPending }) =>
+               isPending ? "pending" : isActive ? "text-red-700 underline " : ""
+             }
+            > Statistics</NavLink>
          </div>
         </div>
     );
